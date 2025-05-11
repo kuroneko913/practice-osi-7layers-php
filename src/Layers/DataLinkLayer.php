5492306ea3731e80f9f81ae667e373b4145420b4
kuroneko913 <myblackcat7112+github@gmail.com>
2025-05-12 00:30:53 +0900

    [fix] Frameが揃うまでバッファリングするように修正 FrameBuilder経由でフレーム構築処理を呼ぶように変更

--- src/Layers/DataLinkLayer.php
+++ src/Layers/DataLinkLayer.php
@@ -2,22 +2,45 @@
 
 namespace App\Layers;
 
+use App\Layers\DataLinkLayer\FrameBuilderInterface;
+
 class DataLinkLayer implements LayerInterface
 {
-    private $lowerLayer;
-
-    public function __construct(LayerInterface $lowerLayer)
-    {
-        $this->lowerLayer = $lowerLayer;
-    }
+    /**
+     * コンストラクタ
+     * @param LayerInterface $lowerLayer
+     * @param FrameBuilderInterface $frameBuilder
+     */
+    public function __construct(
+        private LayerInterface $lowerLayer,
+        private FrameBuilderInterface $frameBuilder
+     ) {}
 
     public function send(mixed $data): void
     {
-        $this->lowerLayer->send($data);
+        static $buffer = '';
+        $buffer .= $data;
+        if (str_ends_with($buffer, "\n")) {
+            $frame = $this->frameBuilder->buildFrame(trim($buffer));
+            $this->lowerLayer->send($frame);
+            $buffer = '';
+        }
     }
 
     public function receive(): mixed
     {
-        return $this->lowerLayer->receive();
+        $buffer = '';
+        while (true) {
+            $raw = $this->lowerLayer->receive();
+            $buffer .= $raw;
+            if (!$this->frameBuilder->isFrameEnd($buffer)) {
+                continue;
+            }
+            // フレームが完成したらパースする
+            $frame = $this->frameBuilder->parseFrame($buffer);
+            $frameLength = $this->frameBuilder->getFrameLength($buffer);
+            $buffer = substr($buffer, $frameLength);
+            return $frame;
+        }
     }
 }
